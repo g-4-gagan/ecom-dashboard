5616857210eeee76b082cea7f2d12be301aee5b3
Gagan <gagansoni4321@gmail.com>
2021-05-21 02:20:08 +0530

    routing in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import Header  from './Header';
+import Login  from './Login';
+import Register  from './Register';
+import AddProduct  from './AddProduct';
+import UpdateProduct  from './UpdateProduct';
+import {BrowserRouter,Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello world!</h1>
-      <button>Normal button</button>
-      <Button>Bootstrap Button</Button>
+      <BrowserRouter>
+      <Header/>
+      <h1>Hello World!</h1>
+      <Route path="/add">
+        <AddProduct/>
+      </Route>
+      <Route path="/update">
+        <UpdateProduct/>
+      </Route>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/register">
+        <Register/>
+      </Route>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Register.js
@@ -0,0 +1,11 @@
+function Register(){
+	return(
+
+	<div>
+		<h1>Regitration Page</h1>
+	</div>
+
+	);
+}
+
+export default Register
