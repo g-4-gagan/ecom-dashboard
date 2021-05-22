3a935c915e1264449bcc4573b4f6be8850d05a43
Gagan <gagansoni4321@gmail.com>
2021-05-23 03:12:16 +0530

    login and add product api integration

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,14 +1,47 @@
 import Header  from './Header';
+import React,{useState,useEffect} from 'react';
 
 function AddProduct(){
 
+	const[name,setName]=useState("");
+	const[file,setFile]=useState("");
+    const[price,setPrice]=useState("");
+    const[description,setDescription]=useState("");
+
+	async function addProduct()
+	{
+		let product ={name,file,price,description};
+		const formData = new FormData;
+		formData.append('name',name);
+		formData.append('file',file);
+		formData.append('price',price);
+		formData.append('description',description);
+
+		let result = await fetch("http://ecom-backend.example.com/api/addProduct",{
+            method:'POST',
+            headers:{
+                "Accept":"application/json"
+            },
+            body: formData
+        });
+        result = await result.json();
+        alert(result.status);
+
+	}
 
 	return(
 
-	<div>
-	    <Header/>
+	<>
+    <Header/>
+	<div className="col-sm-6 offset-sm-3">
 		<h1>Add Product Page</h1>
+		<input type="text" placeholder="Product Name" value={name} onChange= {(e)=>setName(e.target.value)} className="form-control" /><br/>
+		<input type="file" placeholder="Product Image" onChange= {(e)=>setFile(e.target.files[0])} className="form-control" /><br/>
+		<input type="text" placeholder="Product Description" value={description} onChange= {(e)=>setDescription(e.target.value)} className="form-control" /><br/>
+		<input type="text" placeholder="Product Price" value={price} onChange= {(e)=>setPrice(e.target.value)} className="form-control" /><br/>
+		<button onClick ={addProduct} className="btn btn-primary"> Add Product </button>
 	</div>
+	</>
 
 	);
 }
--- src/Login.js
+++ src/Login.js
@@ -10,13 +10,48 @@ function Login(){
     }
     },[]);
     const history=useHistory();
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=useState("");
+
+    async function signIn(){
+      let item ={email,password};
+      
+      let result = await fetch("http://ecom-backend.example.com/api/login",{
+      method:'POST',
+      headers:{
+      "Content-Type":"application/json",
+      "Accept":"application/json"
+       },
+       body: JSON.stringify(item)
+    });
+
+    result = await result.json();
+    if (result.error) 
+    {
+    	alert (result.error[0]);
+    }
+    else
+    {
+    	localStorage.setItem("user-info",JSON.stringify(result));
+    	history.push('/add');
+    }
+
+    }
 
 	return(
 
-	<div>
-	    <Header/>
+	<>
+    <Header/>
+	<div className="col-sm-6 offset-sm-3">
 		<h1>Login Page</h1>
+		<input type="text" placeholder="Email" value={email} onChange= {(e)=>setEmail(e.target.value)}  className="form-control" />
+		<br/>
+		<input type="text" placeholder="password" value={password} onChange= {(e)=>setPassword(e.target.value)}  className="form-control" />
+		<br/>
+		<button onClick ={signIn} className="btn btn-primary"> Sign In </button>
+
 	</div>
+    </>
 
 	);
 }
