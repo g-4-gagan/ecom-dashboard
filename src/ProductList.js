89d15980eb280aea56600cd4afa7d1cb5a6a1b92
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2021-05-24 21:40:22 +0530

    delete api integration

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,16 +1,33 @@
 import Header  from './Header';
 import React,{useState,useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 import {Table} from 'react-bootstrap';
 
 function ProductList(){
 
 	const [data,setData] = useState([]);
-	useEffect(async()=>{
-		let result = await fetch("http://ecom-backend.example.com/api/productList");
-		result = await result.json();
-		setData(result);
+  const history=useHistory();
+	useEffect(()=>{
+		getData();
 	},[]);
-	console.warn("data",data);
+
+  async function deleteOperation(id)
+  {
+    let result = await fetch("http://ecom-backend.example.com/api/deleteProduct/"+id,{
+            method:'DELETE'
+        });
+    result = await result.json();
+    alert(result.status);
+    getData();
+    // history.push('/');
+  }
+  async function getData()
+  {
+    let result = await fetch("http://ecom-backend.example.com/api/productList");
+    result = await result.json();
+    setData(result);
+  }
+
 	return(
 	<>
     <Header/>
@@ -24,18 +41,20 @@ function ProductList(){
            <th>Image</th>
            <th>Description</th>
            <th>Price</th>
+           <th>Operation</th>
        </tr>
        </thead>
        <tbody>
         {
        	data.map((item)=>
        
-           <tr>
+           <tr key={item.id}>
                <td>{item.id}</td>
                <td>{item.name}</td>
                <td><img style={{width: 140}} src={"http://ecom-backend.example.com/"+item.file_path}/></td>
                <td>{item.description}</td>
                <td>{item.price}</td>
+               <td><span className='delete' onClick={()=>{deleteOperation(item.id)}}>Delete</span></td>
            </tr>)
         }
        </tbody>
